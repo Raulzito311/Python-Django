a0c6576e83ff590ceea8bc863ef7b1991bc09548
Raulzito311 <raulmff@gmail.com>
2021-11-26 13:34:54 -0300

    Templates Loops

--- meuProjeto/meuProjeto/views.py
+++ meuProjeto/meuProjeto/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render
 def home(request):
     sexo = 'm'
     nome = 'Diogro'
-    return render(request, 'index.html', {'sex' : sexo, 'name' : nome})
+    lista = [
+        {'nome': 'Pedro', 'sexo' : 'm'},
+        {'nome': 'Maria', 'sexo' : 'f'},
+        {'nome': 'Joaquina', 'sexo' : 'u'},
+        {'nome': 'Joao', 'sexo' : 'm'},
+    ]
+    data = {'sexo' : sexo, 'nome' : nome, 'lista' : lista}
+    return render(request, 'index.html', data)
