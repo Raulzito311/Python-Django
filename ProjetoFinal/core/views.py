0baef79e62164ec240c3f5d336a98ffa3705b787
Raulzito311 <raulmff@gmail.com>
2021-11-29 16:59:34 -0300

    Listagem de Pessoa

--- ProjetoFinal/core/views.py
+++ ProjetoFinal/core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Pessoa
 
 def home(request):
     context = {'mensagem': 'Ola mundo...'}
     return render(request, 'core/index.html', context)
+
+
+def lista_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas})
