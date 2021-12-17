303366d45cc6d477b85383628970d73a7b28a657
Raulzito311 <raulmff@gmail.com>
2021-12-17 17:36:43 -0300

    Adicionando Contatos interativo

--- ProjetoFinal/website/models.py
+++ ProjetoFinal/website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Contato(models.Model):
+    email = models.EmailField()
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    mensagem = models.TextField()
+    receber_noticias = models.BooleanField()
+
+    def __str__(self):
+        return self.nome
--- ProjetoFinal/website/views.py
+++ ProjetoFinal/website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Contato
 
 
 def home(request):
@@ -6,7 +7,27 @@ def home(request):
 
 
 def contato(request):
-    return render(request, 'website/contato.html')
+    status = ''
+    mensagem = ''
+    if request.method == 'POST':
+        try:
+            contato = {}
+            contato['email'] = request.POST.get('email')
+            contato['nome'] = request.POST.get('nome')
+            contato['sobrenome'] = request.POST.get('sobrenome')
+            contato['endereco'] = request.POST.get('endereco')
+            contato['mensagem'] = request.POST.get('mensagem')
+            contato['receber_noticias'] = True if request.POST.get('receber-noticias') == 'on' else False
+
+            Contato.objects.create(**contato)
+        except Exception as e:
+            status = 'show'
+            mensagem = str(e)
+        else:
+            status = 'show'
+            mensagem = 'Contato realizado com sucesso'
+
+    return render(request, 'website/contato.html', {'status' : status, 'mensagem' : mensagem})
 
 
 def servicos(request):
