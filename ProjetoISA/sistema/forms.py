543b66ae0cf523b91efe673ca537dbc5dff193c2
Raulzito311 <raulmff@gmail.com>
2022-02-21 12:37:23 -0300

    Remove old project

--- ProjetoISA/sistema/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm, fields, models
-from .models import (
-    Questionario
-)
-
-class QuestionarioForm(ModelForm):
-    class Meta:
-        model = Questionario
-        fields = '__all__'
